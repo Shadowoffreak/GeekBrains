54db4e6c31369dc892b5608db3e8a00c4bfa2415
BigAF <shadowfareed@yandex.ru>
2021-03-01 15:05:54 +0300

    HW_3

--- HW_1-3/script.js
+++ HW_1-3/script.js
@@ -68,6 +68,78 @@ function getGuessPoints(guess) {
 
 resetGame();
 
-startGame();
+// startGame();
 
 // Задание №1
+// С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+
+let i = 0;
+let maxNumber = 100;
+let primeNumbersCount = [];
+let primeNumbersBase = [2, 3, 5, 7];
+
+nextPrime:
+while (i <= maxNumber) {
+    if (!primeNumbersBase.includes(i)) {
+        let j = 0;
+
+        while(j < primeNumbersBase.length) {
+            if (i % primeNumbersBase[j] === 0 || i === 1) {
+                i++;
+                continue nextPrime;
+            }
+
+            j++;
+        }
+
+    }
+
+    primeNumbersCount.push(i);
+    i++;
+}
+
+console.log(primeNumbersCount);
+
+// Задание №2
+// С этого урока начинаем работать с функционалом интернет-магазина.
+// Предположим, есть сущность корзины. 
+// Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
+// Товары в корзине хранятся в массиве. Задачи:
+// a) Организовать такой массив для хранения товаров в корзине;
+// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+
+const cart = [
+    ['apple', 90, 2],
+    ['banana', 60, 1.5],
+    ['orange', 140, 1],
+    ['lemon', 200, 0.5]
+]
+
+function countBasketPrice (cart) {
+    let resultPrice = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        let countPrice = cart[i][1] * cart[i][2];
+        resultPrice += countPrice;
+    }
+
+    console.log("Итоговая сумма покупок: " + resultPrice + " руб.");
+}
+
+countBasketPrice (cart);
+
+// Задание №3
+// Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла.
+
+for (let i = 0; i < 10; console.log(i++)) {
+    //здесь пусто, надеюсь задание правильно понял;
+}
+
+// Задание №4
+// Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
+
+let part = '';
+
+for (let i = 0; i < 20; i++) {
+    console.log(part += 'x');   
+}
